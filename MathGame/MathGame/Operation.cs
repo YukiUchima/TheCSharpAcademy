1a172fb0193f6a04dc0419f9af51f6318b4cc2de
= <81452155+YukiUchima@users.noreply.github.com>
2024-11-08 09:37:57 -0700

    mathgame DRY refactor required

---
+++ MathGame/MathGame/Operation.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathGame
+{
+    class Operation
+    {
+        public static string add()
+        {
+            var random = new Random();
+            int score = 0;
+            int scorePossible = 0;
+            string playTime = DateTime.Now.ToString("h:mm");
+
+            bool isGameOver = false;
+
+            while (!isGameOver)
+            {
+                int x = random.Next(1, 9);
+                int y = random.Next(1, 9);
+                int ans = x + y;
+
+                Console.WriteLine($"What is the sum of {x} and {y}? ");
+                try
+                {
+                    int userIn = Convert.ToInt32(Console.ReadLine());
+                    if (userIn == ans)
+                    {
+                        Console.WriteLine($"The answer is correct! (Current Score: {++score}/{++scorePossible})");
+                    }
+                    else if (userIn == 0)
+                    {
+                        Console.WriteLine("Thank you for playing!");
+                        isGameOver = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Incorrect! The answer is {ans}. (Current Score: {score}/{++scorePossible})");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid input, let's try a new problem...");
+                    continue;
+                }
+
+            }
+
+            return $"Time({playTime}) - 'Add' Game Score: {score}/{scorePossible}";
+        }
+
+        public static string subtract()
+        {
+            var random = new Random();
+            int score = 0;
+            int scorePossible = 0;
+            string playTime = DateTime.Now.ToString("h:mm");
+
+            bool isGameOver = false;
+
+            while (!isGameOver)
+            {
+                int x = random.Next(1, 9);
+                int y = random.Next(1, 9);
+                int ans = x - y;
+
+                Console.WriteLine($"What is the difference of {x} and {y}? ");
+                try
+                {
+                    int userIn = Convert.ToInt32(Console.ReadLine());
+                    if (userIn == ans)
+                    {
+                        Console.WriteLine($"The answer is correct! (Current Score: {++score}/{++scorePossible})");
+                    }
+                    else if (userIn == 0)
+                    {
+                        Console.WriteLine("Thank you for playing!");
+                        isGameOver = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Incorrect! The answer is {ans}. (Current Score: {score}/{++scorePossible})");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid input, let's try a new problem...");
+                    continue;
+                }
+
+            }
+
+            return $"Time({playTime}) - 'Subtract' Game Score: {score}/{scorePossible}";
+        }
+
+        public static string mult()
+        {
+            var random = new Random();
+            int score = 0;
+            int scorePossible = 0;
+            string playTime = DateTime.Now.ToString("h:mm");
+
+            bool isGameOver = false;
+
+            while (!isGameOver)
+            {
+                int x = random.Next(1, 9);
+                int y = random.Next(1, 9);
+                int ans = x * y;
+
+                Console.WriteLine($"What is the product of {x} and {y}? ");
+                try
+                {
+                    int userIn = Convert.ToInt32(Console.ReadLine());
+                    if (userIn == ans)
+                    {
+                        Console.WriteLine($"The answer is correct! (Current Score: {++score}/{++scorePossible})");
+                    }
+                    else if (userIn == 0)
+                    {
+                        Console.WriteLine("Thank you for playing!");
+                        isGameOver = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Incorrect! The answer is {ans}. (Current Score: {score}/{++scorePossible})");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid input, let's try a new problem...");
+                    continue;
+                }
+
+            }
+
+            return $"Time({playTime}) - 'Multiply' Game Score: {score}/{scorePossible}";
+        }
+
+        public static string div()
+        {
+            var random = new Random();
+            int score = 0;
+            int scorePossible = 0;
+            string playTime = DateTime.Now.ToString("h:mm");
+
+            bool isGameOver = false;
+
+            while (!isGameOver)
+            {
+                int x, y;
+                do
+                {
+                    x = random.Next(0, 100);
+                    y = random.Next(1, 100);
+                }
+                while (x % y != 0);
+                int ans = x / y;              
+
+                Console.WriteLine($"What is the product of {x} and {y}? ");
+                try
+                {
+                    int userIn = Convert.ToInt32(Console.ReadLine());
+                    if (userIn == ans)
+                    {
+                        Console.WriteLine($"The answer is correct! (Current Score: {++score}/{++scorePossible})");
+                    }
+                    else if (userIn == 0)
+                    {
+                        Console.WriteLine("Thank you for playing!");
+                        isGameOver = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Incorrect! The answer is {ans}. (Current Score: {score}/{++scorePossible})");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid input, let's try a new problem...");
+                    continue;
+                }
+
+            }
+
+            return $"Time({playTime}) - 'Divide' Game Score: {score}/{scorePossible}";
+        }
+
+    }   
+}
---
+++ MathGame/MathGame/UserInteraction.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathGame
+{
+    class UserInteraction
+    {
+        private static List<string> gameHistory = [];
+        public static void start()
+        {
+            Console.WriteLine("This is the math game!");
+            bool keepPlaying = true;
+            do
+            {
+                int choice = MenuChoice();
+                keepPlaying = PickOperation(choice);
+            }
+            while (keepPlaying);
+        }
+
+        private static int MenuChoice()
+        {
+            List<int> choiceList = [0, 1, 2, 3, 4, 5];
+            int optionValue;
+
+            Console.WriteLine("\nPick Your Game Mode!");
+            Console.WriteLine("[1] - Add\n[2] - Subtract\n[3] - Multiply\n[4] - Divide\n[5] - Preview Game History\n[0] - EXIT");
+
+            // Requests user input until valid input has been chosen
+            while (true)
+            {
+                try
+                {
+                    optionValue = Convert.ToInt32(Console.ReadLine());
+                    if (!choiceList.Contains(optionValue))
+                    {
+                        throw new Exception();
+                    }
+                    return optionValue;
+                }
+                catch (Exception ex)
+                {
+                    string msg = ex.Message;
+                    Console.WriteLine($"Invalid user input, try again:");
+                    continue;
+                }
+            }
+        }
+
+        
+        private static bool PickOperation(int choice)
+        {
+            if (choice == 5)
+            {
+                PreviewHistory();
+                return true;
+            }
+            else if (choice == 0)
+            {
+                Console.WriteLine("Exiting Game.");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("To exit, enter '0'... Good luck!");
+                switch (choice)
+                {
+                    case 1:
+                        gameHistory.Add(Operation.add());
+                        break;
+                    case 2:
+                        gameHistory.Add(Operation.subtract());
+                        break;
+                    case 3:
+                        gameHistory.Add(Operation.mult());
+                        break;
+                    case 4:
+                        gameHistory.Add(Operation.div());
+                        break;
+                    default:
+                        Console.WriteLine("You've decided to exit the game. Thanks for playing!");
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        public static void PreviewHistory()
+        {
+            if (gameHistory.Count > 0)
+            {
+                Console.WriteLine("\nPreviewing History\n---------------[Start History]---------------");
+                foreach (var history in gameHistory)
+                {
+                    Console.WriteLine(history);
+                }
+                Console.WriteLine("----------------[End History]----------------\n");
+            }
+            else
+            {
+                Console.WriteLine("\n No History Preview Available...\n");
+            }
+        }
+    }
+}
