35c3f2938a15332abb4fc5302cc917c7da7b18c6
= <81452155+YukiUchima@users.noreply.github.com>
2024-12-01 09:44:16 -0700

    update repo

--- Calculator.YukiUchima/CalculatorLibrary/Calculator.cs
+++ Calculator.YukiUchima/CalculatorLibrary/Calculator.cs
@@ -23,7 +23,10 @@ public class Calculator
     public double DoOperation(double num1, Enums.OperationType op, double? num2 = null)
     {
         double? result = double.NaN;
-        
+
+        Func<double, double?, double> sumOp = (x, y) =>  x + y ?? 0;
+        Func<int, int, int> multiplyOp = (x, y) =>  x * y ;
+
         if (!num2.HasValue)
         {
             writer.WriteStartObject();
@@ -65,7 +68,7 @@ public class Calculator
             switch (op)
             {
                 case Enums.OperationType.Add:
-                    result = num1 + num2;
+                    result = sumOp(num1, num2);
                     writer.WriteValue("Add");
                     break;
                 case Enums.OperationType.Subtract:
