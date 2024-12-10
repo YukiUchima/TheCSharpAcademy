66cd0ea8d5267c0418da23686701016764d8ca02
Yuki Uchima <yuki.uchima@educationatwork.org>
2024-12-09 17:23:53 -0700

    add gitignore, start views section

---
+++ cSharpUltimate/ViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ViewsExample.Models;
+
+namespace ViewsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("home")]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewData["appTitle"] = "Asp.Net Core Demo App";
+            List<Person> people = new List<Person>()
+            {
+            new Person(){ Name = "Joe", DateOfBirth = DateTime.Parse("2000-05-06"), GenderType = Person.Gender.Male},
+            new Person(){ Name = "Jane", DateOfBirth = DateTime.Parse("2000-05-06"), GenderType = Person.Gender.Female},
+            new Person(){ Name = "JaneJoe", DateOfBirth = DateTime.Parse("2000-05-06"), GenderType = Person.Gender.Other}
+            };
+
+            ViewData["people"] = people;
+            return View();  // Views/Home/Index.cshtml
+            //return View("abc"); // Views/Home/abc.cshtml
+            //return new ViewResult() { ViewName = "abc" };
+        }
+    }
+}
---
+++ cSharpUltimate/ViewsExample/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace ViewsExample.Models;
+
+public class Person
+{
+    public string? Name { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    public Gender GenderType { get; set; }
+
+    public enum Gender
+    {
+        Male, Female, Other
+    }
+}
