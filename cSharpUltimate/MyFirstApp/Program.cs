4fcdfe6c34d21dac8678021109aefa0ce1f3c1fe
= <81452155+YukiUchima@users.noreply.github.com>
2024-12-09 21:28:39 -0700

    myFirstApp

--- cSharpUltimate/MyFirstApp/Program.cs
+++ cSharpUltimate/MyFirstApp/Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+
+app.Run(async (HttpContext context) =>
+{
+    context.Response.StatusCode = 500; // bad response
+    await context.Response.WriteAsync("Hello, this is the response body\n");
+    await context.Response.WriteAsync("Previously message has finished...");
+});
 
 app.Run();
